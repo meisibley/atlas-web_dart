356d1d326094f5a9f2182aaaa67f833859f0b8f0
meisibley <mkong99@yahoo.com>
2024-10-08 08:54:00 -0500

    task4 - outer inner functions

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,6 @@
 void outer(String name, String id) {
   String inner() {
-    return 'Hello Agent ${name.split(' ')[1][0]}.${name.split(' ')[0]}, your id is $id';
+    return 'Hello Agent ${name.split(' ')[1][0]}.${name.split(' ')[0]} your id is $id';
   }
   print(inner());
 }
