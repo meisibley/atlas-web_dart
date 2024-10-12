bc051274fe78e1730fb8324da241a687d7da603e
meisibley <mkong99@yahoo.com>
2024-10-12 09:25:52 -0500

    task4 - get & set

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,22 @@
+class Password {
+  String _password = '';
+  Password({required String password}) : _password = password;
+  String get password => _password;
+  set password(String password) {
+    _password = password;
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    bool hasUppercase = _password.contains(RegExp(r'[A-Z]'));
+    bool hasLowercase = _password.contains(RegExp(r'[a-z]'));
+    bool hasNumber = _password.contains(RegExp(r'[0-9]'));
+    return hasUppercase && hasLowercase && hasNumber;
+  }
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
